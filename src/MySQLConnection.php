f2cbad837b32e27981b5004f791a21c2b061f86c
João Gabriel <joao.silva2714@etec.sp.gov.br>
2022-11-18 17:44:00 +0000

    Mysql Config

--- src/MySQLConnection.php
+++ src/MySQLConnection.php
@@ -2,7 +2,8 @@
 namespace ExemploPDOMySQL;
 
 class MySQLConnection extends \PDO {
-    public function _construct() {
-        parent::_construct('mysql:host=localhost;dbname=biblioteca', 'root', '');
+    public function __construct() {
+        parent::__construct('mysql:host=localhost;dbname=biblioteca', 'root', '');
     }
 }
+?>
